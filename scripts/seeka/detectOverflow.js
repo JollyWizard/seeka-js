1aeec63dcc7cde0df55285ddc762b1ad9ca9a58c
James Arlow <james.arlow@gmail.com>
2015-03-23 00:18:47 -0400

    detectOverflow detects content overflow usin detectTrailingScrollPadding
    Test and module updated.
    
    Also, comments.

--- scripts/seeka/detectOverflow.js
+++ scripts/seeka/detectOverflow.js
@@ -1,13 +1,30 @@
 /**
 Provides detection for whether an element is overflowing.  
 
-An element is considered overflowing if a scroll is possible on the target axis.
+An element can be considered overflowing in two different scenarios.
 
-NOTES:
-Because of the browser inconsistency regarding in how scroll content is rendered,
-the content can overflow into padding area, but without the elment being unscrollable.
-TODO Add methods content{Width|Height}() to detect these scenarios for responsive design.
+1. If a scroll is possible on the target axis,
+   implying the scrollable content overflows the container.
+   This is referred to as scroll overflow.
 
+2. The content extends beyond the content box into the padding area, 
+   but without the element being scrollable.
+   This is referred to as content overflow.
+
+ The default syntax detects a scroll overflow.
+ Explicit syntax is provided to test content overflow.
+
+@return an object whose methods test the input element
+
+.height() 
+   : returns true if the element is scrollable vertically
+.width() 
+   : returns true if the element is scrollable horizontally
+
+.contentHeight() 
+   : returns true if the content overflows into the bottom padding area.
+.contentWidth() 
+   : returns true if the content overflows into the padding area.
 */
 function detectOverflow(element) {
 	var target = element.jquery ? element[0] : element
@@ -36,17 +53,23 @@ function detectOverflow(element) {
 			return true
 		}
 	,	contentWidth : function() {
-			var css = getComputedStyle(element)
+			var css = getComputedStyle(target)
 			var width = parseInt(css.width,10)
-			var padding = parseInt(css.paddingRight,10)
+			
+			var padding = parseInt(css.paddingLeft,10)
+				+ detectTrailingScrollPadding() ? parseInt(css.paddingRight,10) : 0
 			
 			return element.scrollWidth - padding > width
 		}	
 	,	contentHeight : function() {
-			var css = getComputedStyle(element).height
-			css = parseInt(css,10)
+			var css = getComputedStyle(target)
+			var height = parseInt(css.height,10)
 			
-			return element.scrollHeight > css
+			var padding = parseInt(css.paddingTop, 10) 
+				+ detectTrailingScrollPadding() ? parseInt(css.paddingBottom,10) : 0
+			
+			return element.scrollHeight - padding > height
 		}
 	}
 }
+	
